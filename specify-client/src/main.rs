198ad38a6e01e017f12ecfc6b7a427ed7b42ffe0
SealsRock12 <sign@toad39.dev>
2022-08-26 20:42:17 -0700

    (client): config

--- specify-client/src/main.rs
+++ specify-client/src/main.rs
@@ -1,8 +1,11 @@
 mod data;
+mod config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:#?}", data::get_cimos()?);
     println!("{:#?}", data::get_avfw()?);
+
+    println!("{:#?}", config::BAD_PROCESSES);
 
     Ok(())
 }
