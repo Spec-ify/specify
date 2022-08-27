59e97e7867788b778c71b2402a2962a3365de5f7
SealsRock12 <sign@toad39.dev>
2022-08-26 19:06:35 -0700

    (client): basic WMI test

--- specify-client/src/main.rs
+++ specify-client/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+mod data;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("{:#?}", data::get_cimos()?);
+
+    Ok(())
 }
