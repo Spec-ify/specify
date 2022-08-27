79fdae6bf88fc9c9694a7e66d9282304b8e97789
SealsRock12 <sign@toad39.dev>
2022-08-26 19:54:41 -0700

    (client): WMI with namespaces, make functions/types/stuff

--- specify-client/src/main.rs
+++ specify-client/src/main.rs
@@ -1,9 +1,8 @@
-use std::io;
-
 mod data;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:#?}", data::get_cimos()?);
+    println!("{:#?}", data::get_avfw()?);
 
     Ok(())
 }
