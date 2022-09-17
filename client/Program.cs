fb430734c849d5253dd8fefa056f1667b4b623fe
sealsrock12 <sign@toad39.dev>
2022-09-17 11:48:48 -0700

    (client): Use c# now

---
+++ client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Management.Infrastructure;
+
+namespace specify_client;
+
+public class Program
+{
+    static void Main()
+    {
+        var d = new Data();
+
+        var e = d.GetWmi();
+
+        foreach (CimProperty p in e)
+        {
+            Console.WriteLine("{0} = {1}", p.Name, p.Value);
+        }
+    }
+}
+
