a82a727a6eb75ff0c95a48acacf76d2311680bd0
sealsrock12 <sign@toad39.dev>
2022-09-17 11:48:48 -0700

    (client): Use c# now

---
+++ client/Data.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Management.Infrastructure;
+using Microsoft.Management.Infrastructure.Generic;
+
+namespace specify_client;
+
+public class Data
+{
+    private CimSession session;
+    private const string DefaultNs = @"root\cimv2";
+
+    public Data()
+    {
+        session = CimSession.Create(null);
+    }
+
+    public CimKeyedCollection<CimProperty> GetWmi()
+    {
+        return session.GetInstance(DefaultNs, new CimInstance("Win32_OperatingSystem")).CimInstanceProperties;
+    }
+}
