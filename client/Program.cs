336945087f9416ae8283d000d03180e30c9b6579
sealsrock12 <sign@toad39.dev>
2022-09-18 17:31:33 -0700

    (client): WMI result pretty printing

--- client/Program.cs
+++ client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Management;
 using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace specify_client;
 
@@ -20,5 +21,21 @@ public class Program
         var jsonString = JsonConvert.SerializeObject(o, Formatting.Indented);
 
         Console.WriteLine(jsonString);
+    }
+
+    public static void PrettyPrintWmiResults(List<Dictionary<string, object>> wmi)
+    {
+        foreach (var instance in wmi)
+        {
+            foreach (var pair in instance)
+            {
+                Console.WriteLine("{0} = {1}", pair.Key, pair.Value);
+            }
+
+            if (!instance.Equals(wmi.Last()))
+            {
+                Console.WriteLine("---------------");
+            }
+        }
     }
 }
