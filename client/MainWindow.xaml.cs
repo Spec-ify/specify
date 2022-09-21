2dc11435beb0b284fd5fffeaf867a23d90421ff8
sealsrock12 <sign@toad39.dev>
2022-09-21 15:09:10 -0700

    (client): Make views work better? WPF has consumed my soul, idk what I'm doing anymore

--- client/MainWindow.xaml.cs
+++ client/MainWindow.xaml.cs
@@ -4,46 +4,60 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Newtonsoft.Json;
 
-namespace specify_client
+namespace specify_client;
+
+public class MainWindowViewModel
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public string TestContent { get; set; }
+
+    public MainWindowViewModel()
     {
-        public static SolidColorBrush Red = new SolidColorBrush(Color.FromRgb(0xbf, 0x61, 0x6a));
-        public static SolidColorBrush Nord6 = new SolidColorBrush(Color.FromRgb(0xec, 0xef, 0xf4));
+        TestContent = (new Monolith
+        {
+            BasicInfo = MonolithBasicInfo.Create()
+        }).Serialize();
+    }
+}
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public static SolidColorBrush Red = new SolidColorBrush(Color.FromRgb(0xbf, 0x61, 0x6a));
+    public static SolidColorBrush Nord6 = new SolidColorBrush(Color.FromRgb(0xec, 0xef, 0xf4));
+    private string TestContent { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Control.Template = (ControlTemplate) Resources["StartView"];
+        CloseButton.Background = Brushes.Transparent;
         
-        public MainWindow()
-        {
-            InitializeComponent();
+        DataContext = new MainWindowViewModel();
+    }
 
-            Test.Content = JsonConvert.SerializeObject(MonolithBasicInfo.Create(), Formatting.Indented);
-            CloseButton.Background = Brushes.Transparent;
-        }
+    private void OnCloseClick(object sender, RoutedEventArgs e)
+    {
+        //Test.Content = "Clicked!";
+        this.Close();
+    }
 
-        private void OnCloseClick(object sender, RoutedEventArgs e)
-        {
-            //Test.Content = "Clicked!";
-            this.Close();
-        }
+    private void TitleBar_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
+    }
 
-        private void TitleBar_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
-        }
+    private void CloseButton_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        CloseButton.Background = Red;
+        ClosePath.Fill = Nord6;
+    }
 
-        private void CloseButton_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            CloseButton.Background = Red;
-            ClosePath.Fill = Nord6;
-        }
-
-        private void CloseButton_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            CloseButton.Background = Brushes.Transparent;
-            ClosePath.Fill = Red;
-        }
+    private void CloseButton_OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        CloseButton.Background = Brushes.Transparent;
+        ClosePath.Fill = Red;
     }
 }
