82421479292818e3f73d4f437c61309325965aaa
sealsrock12 <sign@toad39.dev>
2022-09-25 20:53:21 -0700

    (client): fancy progress status things, console not cleaned up yet

--- client/Program.cs
+++ client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Newtonsoft.Json;
 
 namespace specify_client
@@ -10,17 +11,21 @@ namespace specify_client
         static void Main()
         {
             var pList = new ProgressList();
+            pList.PrintStatuses();
             pList.RunItem("Test");
             pList.RunItem("MainData");
-            
+            Thread.Sleep(1000);
+            pList.RunItem("DummyTimer");
+            // pList.RunItem("MainData");
+
             //Console.WriteLine("Last boot up time: " + Data.CimToIsoDate((string) DataCache.Os["LastBootUpTime"]));
             //Console.WriteLine("Time now: " + Data.DateTimeToIsoDate(DateTime.Now));
             //PrettyPrintObject(MonolithBasicInfo.Create());
         }
 
         /**
-     * System.Text.Json doesn't work, so I'm using Newtonsoft.Json
-     */
+         * System.Text.Json doesn't work, so I'm using Newtonsoft.Json
+         */
         public static void PrettyPrintObject(object o)
         {
             var jsonString = JsonConvert.SerializeObject(o, Formatting.Indented);
