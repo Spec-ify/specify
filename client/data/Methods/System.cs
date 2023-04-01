b489c7d3ffca858e8825f2056a8643038e5aa9f3
JimmahDean <JimmahDean@users.noreply.github.com>
2023-04-01 09:43:14 -0600

    Patch to prevent uploading empty zip files on bsod dump copy failure

--- client/data/Methods/System.cs
+++ client/data/Methods/System.cs
@@ -375,13 +375,32 @@ public static partial class Cache
     {
         try
         {
+            bool copied = false;
             //Any dump older than a month is not included in the zip.
             foreach (string dump in dumps)
+            {
                 if (new FileInfo(dump).CreationTime > DateTime.Now.AddMonths(-1))
-                    File.Copy(dump, string.Concat(TempFolder + @"/", Regex.Match(dump, "[^\\\\]*$").Value));
+                {
+                    var fileName = string.Concat(TempFolder + @"/", Regex.Match(dump, "[^\\\\]*$").Value);
+                    File.Copy(dump, fileName);
+                    if (!File.Exists(fileName))
+                    {
+                        await DebugLog.LogEventAsync($"Failed to copy {Regex.Match(dump, "[^\\\\]*$").Value} to dump folder.", DebugLog.Region.System, DebugLog.EventType.ERROR);
+                    }
+                    else
+                    {
+                        copied = true;
+                    }
+                }
+            }
 
-            ZipFile.CreateFromDirectory(TempFolder, TempZip);
-            return true;
+            // If at least one dump was successfully copied, create the directory and return success.
+            if (copied)
+            {
+                ZipFile.CreateFromDirectory(TempFolder, TempZip);
+                return true;
+            }
+            else return false;
         }
         catch (Exception e)
         {
