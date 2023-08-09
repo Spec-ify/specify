7ac5739cf54bf714d50a7bcecabdb80f29f0e1d1
JimmahDean <8887005+JimmahDean@users.noreply.github.com>
2023-08-09 10:28:30 -0600

    Changed version number to v2.0.0

--- client/Program.cs
+++ client/Program.cs
@@ -11,7 +11,7 @@ namespace specify_client;
 
 public class Program
 {
-    public const string SpecifyVersion = "v1.1.2";
+    public const string SpecifyVersion = "v2.0.0";
     public static Stopwatch Time;
 
     public static async Task Main()
