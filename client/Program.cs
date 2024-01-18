166c9aa06ec39a84a65cc0f013d4bc8d8736bb7d
sealsrock12 <sign@toad39.dev>
2024-01-18 07:06:43 -0800

    v1.2.1

--- client/Program.cs
+++ client/Program.cs
@@ -11,7 +11,7 @@ namespace specify_client;
 
 public class Program
 {
-    public const string SpecifyVersion = "v1.2.0";
+    public const string SpecifyVersion = "v1.2.1";
     public static Stopwatch Time;
 
     public static async Task Main()
